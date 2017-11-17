e8065d4171a82a98f570d8f39995aacf0c2cba73
Holly Wilkalis <holly.wilkalis@gmail.com>
2017-11-16 22:53:09 -0800

    Add object constructor script

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+function Movie (title, year, score) {
+  this.title = title;
+  this.year = year;
+  this.score = score;
+}
+
 $(document).ready(function() {
   $("form#contact-info").submit(function(event) {
   event.preventDefault();
@@ -5,8 +11,15 @@ $(document).ready(function() {
     var inputtedMovieTitle = $("input#movie-title").val();
     var inputtedMovieYear = $("input#movie-year").val();
     var inputtedMovieScore = $("input#movie-score").val();
+    var Movies = [];
 
-    $("#movie-table").append("<tr><td>" + inputtedMovieTitle + "</td><td>" + inputtedMovieYear + "</td><td>" + inputtedMovieScore + "</td></tr>");
+    var newMovie = new Movie(inputtedMovieTitle, inputtedMovieYear, inputtedMovieScore);
+    // var sortedMovies = Movies.slice().sort()
+    // console.log(sortedMovies)
+
+    $("#movie-table").append("<tr><td>" + newMovie.title + "</td><td>" + newMovie.year + "</td><td>" + newMovie.score + "</td></tr>");
   });
+
+
 
 });
